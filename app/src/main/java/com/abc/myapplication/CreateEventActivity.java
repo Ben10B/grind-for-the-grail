6746a5173776bd19f0df72857ae7ab7454fe22ab
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-05-30 15:02:54 -0600

    hkjjl

--- app/src/main/java/com/abc/myapplication/CreateEventActivity.java
+++ app/src/main/java/com/abc/myapplication/CreateEventActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class CreateEventActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +17,22 @@ public class CreateEventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_event);
     }
     public void createDungeon(View view){
-        EditText text = findViewById(R.id.goalText);
-        String goal = text.getText().toString();
+        EditText gT = findViewById(R.id.goalText);
+        EditText dT = findViewById(R.id.dateText);
+        EditText rT = findViewById(R.id.rewardText);
+        EditText pT = findViewById(R.id.punishmentText);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        RadioButton button = findViewById(radioId);
+
+        String goal = gT.getText().toString();
+        String date = dT.getText().toString();
+        String reward = rT.getText().toString();
+        String penalty = pT.getText().toString();
+        String diff = button.getText().toString();
+
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+//        databaseHelper.addDungeon(4, 7, diff, penalty,reward, "sex", "grreat sex", "null", );
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("GOAL", goal);
