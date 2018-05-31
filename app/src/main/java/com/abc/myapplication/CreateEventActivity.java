e58d8d2a05cff63e29464134c410406223380c68
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-05-31 13:47:24 -0600

    i am god

--- app/src/main/java/com/abc/myapplication/CreateEventActivity.java
+++ app/src/main/java/com/abc/myapplication/CreateEventActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import Database.DatabaseHelper;
+
 public class CreateEventActivity extends AppCompatActivity {
 
 
@@ -36,8 +38,9 @@ public class CreateEventActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("GOAL", goal);
-
-        startActivity(intent);
-        finish();
+//        if(goal.length() == 0){
+            startActivity(intent);
+            finish();
+//        }
     }
 }
