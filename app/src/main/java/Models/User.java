a2692137e5766ea314a36103f5b9dfb58b523057
SamKnow <sknowlton@student.neumont.edu>
2018-06-02 14:44:25 -0600

    Made Model Classes Parcelable
    Also added constructors.

--- app/src/main/java/Models/User.java
+++ app/src/main/java/Models/User.java
@@ -1,33 +1,90 @@
 package Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
-public class User {
+import Database.DatabaseHelper;
+
+public class User implements Parcelable{
+    public User(String username, String email, Sprite sprite, Dungeon[] dungeons) {
+        this.username = username;
+        this.email = email;
+        this.sprite = sprite;
+        this.dungeons = dungeons;
+    }
+
     private String username;
-    private String password;
     private String email; //<<stretch>>
     private Sprite sprite;
-    private ArrayList<Dungeon> dungeons;
+    private Dungeon[] dungeons;
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 
     public User(String username, String password) {
         this.username = username;
-        this.password = password;
-        dungeons = new ArrayList<Dungeon>();
+        this.dungeons = new Dungeon[5];
         this.sprite = new Sprite(username);
+    }
+
+    public User(Parcel in){
+        this.username = in.readString();
+        this.email = in.readString();
+        this.sprite =  in.readParcelable(Sprite.class.getClassLoader());
+        Object[] ar = in.readArray(Dungeon.class.getClassLoader());
+        this.dungeons = Arrays.copyOf(ar,ar.length,Dungeon[].class);
+        //TODO This might break!
     }
 
     public void addDungeon(String title, Date endDate){
         Dungeon temp = new Dungeon(title, sprite.getMaxHealth(), endDate);
-        ArrayList<Dungeon> dungeonTemp = getDungeons();
-        dungeonTemp.add(temp);
-        setDungeons(dungeonTemp);
+        Dungeon[] oldArr = getDungeons();
+        Dungeon[] newArr = new Dungeon[oldArr.length + 1];
+        for(int i = 0; i < oldArr.length; i++){
+            newArr[i] = oldArr[i];
+        }
+        newArr[oldArr.length] = temp;
+        setDungeons(newArr);
     }
 
-    public boolean removeDungeon(Dungeon dungeon){
-        ArrayList<Dungeon> dungeonList = getDungeons();
-        boolean removed = dungeonList.remove(dungeon);
-        return removed;
+    public void removeDungeon(Dungeon dungeon){
+        Dungeon[] oldArr = getDungeons();
+        Dungeon[] newArr = new Dungeon[oldArr.length - 1];
+        int j = 0;
+        for(int i = 0; i < oldArr.length; i++){
+            if(oldArr[i] == dungeon){
+                j--;
+            }
+            else {
+                newArr[i] = oldArr[i];
+            }
+            j++;
+        }
+        setDungeons(newArr);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getUsername());
+        dest.writeString(getEmail());
+        getSprite().writeToParcel(dest,0);
+        dest.writeArray(getDungeons());
+        //TODO This might break!
     }
 
     //Getters and Setters beyond this point
@@ -37,14 +94,6 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEmail() {
@@ -63,11 +112,11 @@ public class User {
         this.sprite = sprite;
     }
 
-    public ArrayList<Dungeon> getDungeons() {
+    public Dungeon[] getDungeons() {
         return dungeons;
     }
 
-    public void setDungeons(ArrayList<Dungeon> dungeons) {
+    public void setDungeons(Dungeon[] dungeons) {
         this.dungeons = dungeons;
     }
 }
