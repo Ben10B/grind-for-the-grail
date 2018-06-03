9f80bb71a0aaaa2b6f24c096317c396edeefa8a4
Benjamin Beckwith <bbeckwith@student.neumont.edu>
2018-06-03 12:22:16 -0600

    I did some validation

--- app/src/main/java/com/abc/myapplication/CreateEventActivity.java
+++ app/src/main/java/com/abc/myapplication/CreateEventActivity.java
@@ -9,10 +9,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.YearMonth;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import Database.DatabaseDungeonContract;
 import Database.DatabaseHelper;
@@ -45,49 +49,60 @@ public class CreateEventActivity extends AppCompatActivity {
         String reward = rT.getText().toString();
         String penalty = pT.getText().toString();
 
-        Date date1 = null;
+        Date endDate = null;
         String diff = "";
         try{
             diff = button.getText().toString();
-            int year = Integer.parseInt(date.substring(0, 3));
-            int month = Integer.parseInt(date.substring(5, 6));
-            int day = Integer.parseInt(date.substring(8, 9));
 
-            date1 = new Date(year, month, day);
+            int year = Integer.parseInt(date.substring(6, 10));
+            int month = Integer.parseInt(date.substring(0, 2)) - 1;
+            int day = Integer.parseInt(date.substring(3, 5));
+
+            Calendar now = Calendar.getInstance();
+            int currentYear = now.get(Calendar.YEAR);
+            if(year >= currentYear){
+                if(month < 12 && month > 0){
+                    Calendar days = new GregorianCalendar(year, month, 1);
+                    int daysInMonth = days.getActualMaximum(Calendar.DAY_OF_MONTH);
+                    if(day <= daysInMonth){
+                        endDate = new Date(year, month, day);
+                    }
+                }
+            }
+
         } catch(Exception e){}
 
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
-        Cursor spriteCursor = databaseHelper.readAllSprites(databaseHelper.getReadableDatabase());
-        spriteCursor.moveToFirst();
-        int maxhealth = spriteCursor.getInt(spriteCursor.getColumnIndex(DatabaseSpriteContract.ContractEntry.MAXHEALTH));
-        spriteCursor.close();
-        SQLiteDatabase database = databaseHelper.getWritableDatabase();
-        Dungeon dungeon = new Dungeon(goal,maxhealth,date1,Difficulty.valueOf(diff));
-        databaseHelper.addDungeon(goal,maxhealth, maxhealth, diff, penalty,reward, "", "", null, database);
-        Cursor dungeonCursor = databaseHelper.readAllDungeons(databaseHelper.getReadableDatabase());
-        int dungeonid = 0;
-        if (dungeonCursor.moveToFirst()) {
-            while (!dungeonCursor.isAfterLast()) {
-                if(dungeonCursor.getString(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.NAME)) == goal &&
-                   dungeonCursor.getString(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.DIFFICULTY)) == diff &&
-                   dungeonCursor.getInt(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.MAXHEALTH)) == maxhealth)
-                    dungeonid = dungeonCursor.getInt(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.DUNGEONID));
-                dungeonCursor.moveToNext();
-            }
+        if(!goal.equals("") && !diff.equals("") && date != null){
+//            DatabaseHelper databaseHelper = new DatabaseHelper(this);
+//            Cursor spriteCursor = databaseHelper.readAllSprites(databaseHelper.getReadableDatabase());
+//            spriteCursor.moveToFirst();
+//            int maxhealth = spriteCursor.getInt(spriteCursor.getColumnIndex(DatabaseSpriteContract.ContractEntry.MAXHEALTH));
+//            spriteCursor.close();
+//            SQLiteDatabase database = databaseHelper.getWritableDatabase();
+//            Dungeon dungeon = new Dungeon(goal,maxhealth,endDate,Difficulty.valueOf(diff));
+//            databaseHelper.addDungeon(goal,maxhealth, maxhealth, diff, penalty,reward, "", "", null, database);
+//            Cursor dungeonCursor = databaseHelper.readAllDungeons(databaseHelper.getReadableDatabase());
+//            int dungeonid = 0;
+//            if (dungeonCursor.moveToFirst()) {
+//                while (!dungeonCursor.isAfterLast()) {
+//                    if(dungeonCursor.getString(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.NAME)) == goal &&
+//                            dungeonCursor.getString(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.DIFFICULTY)) == diff &&
+//                            dungeonCursor.getInt(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.MAXHEALTH)) == maxhealth)
+//                        dungeonid = dungeonCursor.getInt(dungeonCursor.getColumnIndex(DatabaseDungeonContract.ContractEntry.DUNGEONID));
+//                    dungeonCursor.moveToNext();
+//                }
+//            }
+//
+//            for (DungeonDate d : dungeon.getDungeonDates()) {
+//                DateFormat dateFormat = new SimpleDateFormat("mm-dd-yyyy hh:mm:ss");
+//                databaseHelper.addDungeonDate(dungeonid,dateFormat.format(d.getDate()),d.getStatus().toString(),databaseHelper.getWritableDatabase());
+//            }
+//            databaseHelper.close();
+//
+//            startActivity(new Intent(this, MainActivity.class));
+//            finish();
+        }else{
+            Toast.makeText(this, "GOAL, DATE, & DIFFICULTY REQUIRED", Toast.LENGTH_LONG).show();
         }
-
-        for (DungeonDate d : dungeon.getDungeonDates()) {
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-            databaseHelper.addDungeonDate(dungeonid,dateFormat.format(d.getDate()),d.getStatus().toString(),databaseHelper.getWritableDatabase());
-        }
-        databaseHelper.close();
-
-        Intent intent = new Intent(this, MainActivity.class);
-//        intent.putExtra("GOAL", goal);
-
-//        if(goal.length() == 0){
-            startActivity(intent);
-            finish();
-//        }
     }
 }
