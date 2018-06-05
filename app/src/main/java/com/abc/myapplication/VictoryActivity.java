b443b93eaa5a3c890183968a23351fc019ae19d2
Unknown <gabebarber94@gmail.com>
2018-06-05 14:34:07 -0600

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/abc/myapplication/VictoryActivity.java
+++ app/src/main/java/com/abc/myapplication/VictoryActivity.java
@@ -1,7 +1,11 @@
 package com.abc.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class VictoryActivity extends AppCompatActivity {
 
@@ -9,5 +13,7 @@ public class VictoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_victory);
+        TextView victoryTextview = findViewById(R.id.victory_textview);
+        victoryTextview.setBackgroundColor(Color.argb(50,0,0,0));
     }
 }
